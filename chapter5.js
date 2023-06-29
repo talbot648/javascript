92d642563b75ce8e94f6bb3cfe2bbfa471acdf6b
Charles Talbot <charles.talbot@bjss.com>
2023-06-29 09:40:30 +0100

    Chaoter 5 random car creator function

--- chapter5.js
+++ chapter5.js
@@ -67,30 +67,81 @@ if (worthALook) {
 }
 
 
-var song = {
-    name: "Walk This Way",
-    artist: "Run-D.M.C",
-    minutes: 4,
-    seconds: 3,
-    genre: "80's",
-    playing: false,
-
-    play: function() {
-        if(!this.playing) {
-            this.playing = true;
-            console.log("Playing " + this.name + " by " + this.artist);
-        }
-    },
 
 
-    pause: function() {
-        if (this.playing) {
-            this.playing = false;
-            console.log(this.name + " Is now paused")
-        }
-    }
+
+
+
+function autoCarCreator() {
+
+    var makes = ["Chevy", "GM", "Fiat", "Webville Motors", "Tucker"];
+    var models = ["Cadillac", "500", "Bel-Air", "Taxi", "Torpedo"];
+    var years = [1955, 1957, 1948, 1954, 1961];
+    var colors = ["red", "blue", "tan", "yellow", "white"];
+    var convertible = [true, false];
+
+    var rand1 = Math.floor(Math.random() * makes.length);
+    var rand2 = Math.floor(Math.random() * models.length);
+    var rand3 = Math.floor(Math.random() * years.length);
+    var rand4 = Math.floor(Math.random() * colors.length);
+    var rand5 = Math.floor(Math.random() * 5);
+    var rand6 = Math.floor(Math.random() * 2);
+
+    var car = {
+        make: makes[rand1],
+        model: models[rand2],
+        year: years[rand3],
+        color: colors[rand4],
+        passengers: rand5,
+        convertible: convertible[rand6],
+        mileage: 0
+    };
+    return car;
 
 }
 
-song.play()
-song.pause();
+function displayCar(car) {
+    console.log("Your new car is a " + car.year + " " + car.make + " " + car.model);
+
+}
+
+var carToSell = autoCarCreator();
+displayCar(carToSell);
+
+
+
+
+
+
+function songPauseAndPlay() {
+
+    var song = {
+        name: "Walk This Way",
+        artist: "Run-D.M.C",
+        minutes: 4,
+        seconds: 3,
+        genre: "80's",
+        playing: false,
+
+        play: function () {
+            if (!this.playing) {
+                this.playing = true;
+                console.log("Playing " + this.name + " by " + this.artist);
+            }
+        },
+
+
+        pause: function () {
+            if (this.playing) {
+                this.playing = false;
+                console.log(this.name + " Is now paused")
+            }
+        }
+
+    }
+
+    song.play()
+    song.pause();
+
+}
+
