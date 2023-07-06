8cbff84ec5d41c3250da1fdf0baaabaad4e320ca
Charles Talbot <charles.talbot@bjss.com>
2023-07-06 10:35:07 +0100

    be the browser

---
+++ battleship.js
@@ -0,0 +1,12 @@
+var view = {
+
+    //this takes a string and displays it in the message display area
+    displayMessage: function(msg){
+            
+    },
+    displayHit: function(location) {
+    },
+    displayMiss: function(location) {
+    }
+
+};
--- chapter5.js
+++ chapter5.js
@@ -113,35 +113,5 @@ displayCar(carToSell);
 
 
 
-function songPauseAndPlay() {
 
-    var song = {
-        name: "Walk This Way",
-        artist: "Run-D.M.C",
-        minutes: 4,
-        seconds: 3,
-        genre: "80's",
-        playing: false,
-
-        play: function () {
-            if (!this.playing) {
-                this.playing = true;
-                console.log("Playing " + this.name + " by " + this.artist);
-            }
-        },
-
-
-        pause: function () {
-            if (this.playing) {
-                this.playing = false;
-                console.log(this.name + " Is now paused")
-            }
-        }
-
-    }
-
-    song.play()
-    song.pause();
-
-}
 
